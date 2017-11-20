a7e63a1dc9915f7d1bc277cb8c5697e1e5e79f64
Alexander Kozlov <alexabderl@coalla.ru>
2017-11-20 16:05:44 +0300

    fix pages navigate + add img

--- dist/js/application.js
+++ dist/js/application.js
@@ -203,6 +203,22 @@ $(function () {
 
     function footerInit() {
 
+        var pathname = window.location.pathname; // Returns path only
+        // var url      = window.location.href;     // Returns full URL
+
+        console.log(pathname);
+
+        if (pathname === '/') {
+
+            $('.js-navigate-item-1').addClass('is-current');
+        } else if (pathname === '/profile.html') {
+
+            $('.js-navigate-item-2').addClass('is-current');
+        } else if (pathname === '/contacts.html') {
+
+            $('.js-navigate-item-3').addClass('is-current');
+        }
+
         var windowHeight = $(window).height();
         var headerHeight = $('.js-site-header').outerHeight();
         var footerHeight = $('.js-site-footer').outerHeight();
